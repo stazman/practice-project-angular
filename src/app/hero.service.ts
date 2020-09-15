7042b2cfba63686c2f4ac9dbae7b9780ab7dc865
Christopher DiStasio <cadistasio@gmail.com>
2020-09-14 19:18:47 -0500

    Edits getHeroes to return heroes via HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,10 +13,9 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-  // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
