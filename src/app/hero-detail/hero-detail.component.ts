b07818c52975d6e1e77e62f9c6e64a2fceb565d6
Christopher DiStasio <cadistasio@gmail.com>
2020-09-22 21:01:25 -0500

    Adds save method

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -21,17 +21,22 @@ export class HeroDetailComponent implements OnInit {
 
 
   ngOnInit(): void {
-  this.getHero();
-}
+    this.getHero();
+  }
 
-getHero(): void {
-  const id = +this.route.snapshot.paramMap.get('id');
-  this.heroService.getHero(id)
-    .subscribe(hero => this.hero = hero);
-}
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 
-goBack(): void {
-  this.location.back();
-}
+  save(): void {
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
