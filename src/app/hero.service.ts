6b7e2c88ca0f28f977bf1f423bffd252b4faac3c
Christopher DiStasio <cadistasio@gmail.com>
2020-09-23 06:20:44 -0500

    Adds addHero service method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -43,6 +43,14 @@ export class HeroService {
     );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
 /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
